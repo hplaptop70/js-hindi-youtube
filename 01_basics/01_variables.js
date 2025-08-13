ed6872b347a7a17f050244bfb36e3847e25a3f82
hplaptop70 <amansaifi952859@gmail.com>
2025-08-13 10:28:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 145698
+let accountEmail = "aman@google@gmail.com"
+var accountPassword = "12345"
+accountCity= "UttarPardesh"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail = "samad@abdul@gmail.com"
+accountPassword = "258741"
+accountCity = "bangluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
