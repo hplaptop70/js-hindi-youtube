ebe0903b215d145dad864e22b4594d92fe32834d
hplaptop70 <amansaifi952859@gmail.com>
2025-08-13 11:08:00 +0000

    Conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "Aman"
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn ="Aman"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 =>> true , 0 => false
+// "" => false
+// "Aman" => true
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber)
