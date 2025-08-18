6e3d49e299d36185dfd5a4ccf85e979ea6f8a36c
hplaptop70 <amansaifi952859@gmail.com>
2025-08-18 09:55:48 +0000

    Arrow Function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,43 @@
+const user = {
+    username : "aman",
+    price : 999,
+    welcomeMessage : function(){
+        console.log((`${this.username} , welcome to website`));
+
+        // console.log(this);
+        
+    }
+
+}
+// user.welcomeMessage()
+// user.username="Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username= "aman"
+//     console.log(this.username);
+    
+
+// }
+
+
+// const chai =() =>{
+//     let username= "aman"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+// const addTwo = (num1,num2)=> {
+//     return num1+num2
+// }
+
+// const addTwo = (num1,num2)=> (num1+num2)
+// const addTwo = (num1,num2)=> num1+num2
+// const addTwo = (num1,num2)=> (username : "aman")
+// const addTwo = (num1,num2)=> ({username : "aman"})
+// console.log(addTwo(3,4));
+
