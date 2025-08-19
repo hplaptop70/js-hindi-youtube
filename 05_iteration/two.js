93588eab7679f8769ff88a2a11b7ded9a83d0ade
hplaptop70 <amansaifi952859@gmail.com>
2025-08-19 09:23:31 +0000

    DoWhile Loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+//while
+
+// let i=1
+// while (i<=10) {
+//     console.log(`Value of i : ${i}`);
+//     i++
+// }
+
+
+
+// let myArray = ["flash" , "superman" , "batman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
